e404bde090fee9613dc0b883553f4442b5c68295
Wick <vic_the_gunners@hotmail.com>
2025-06-17 17:01:06 +0700

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  domains: ["localhost"],
   /* config options here */
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '', // เช่น www.google.com
-        port: '', // ถ้าไม่มี port ให้ใส่ ''
-        pathname: '/uploads/**', // ตัวอย่าง: /uploads/**
-      },
-    ],
-  },
+  // images: {
+  //   domains: ['localhost'],
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: '', // เช่น www.google.com
+  //       port: '', // ถ้าไม่มี port ให้ใส่ ''
+  //       pathname: '/uploads/**', // ตัวอย่าง: /uploads/**
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
